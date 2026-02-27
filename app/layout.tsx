826d9448eeac51ecf91c129e3ed9145d558b653b
Zachary Huff <zach.huff.386@gmail.com>
2026-02-27 09:30:30 -0500

    Update config in app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,54 +1,54 @@
-import type { Metadata, Viewport } from 'next'
-import "./globals.css";
+import type { Metadata, Viewport } from "next"
+import "./globals.css"
 
-import type { ProductDefinition } from './types'
+import type { ProductDefinition } from "./types"
 
-import cloud from './cloud/config'
+import cloud from "./cloud/config"
 
 const products: Record<string, ProductDefinition> = {
-  cloud,
+	cloud,
 }
 
 const product = products[process.env.PRODUCT as string]
 const { config } = product
 
 export const metadata: Metadata = {
-  title: config.title,
-  description: config.description,
-  openGraph: {
-    title: config.title,
-    description: config.description,
-    type: 'website',
-  },
-  twitter: {
-    title: config.title,
-    description: config.description,
-    card: 'summary_large_image',
-  },
-  icons: {
-    apple: "/webclip.png",
-  },
-  other: {
-    "theme-color": "#111827",
-  },
+	title: config.title,
+	description: config.description,
+	openGraph: {
+		title: config.title,
+		description: config.description,
+		type: "website",
+	},
+	twitter: {
+		title: config.title,
+		description: config.description,
+		card: "summary_large_image",
+	},
+	icons: {
+		apple: "/webclip.png",
+	},
+	other: {
+		"theme-color": "#111827",
+	},
 }
 
 export const viewport: Viewport = {
-  width: "device-width",
-  initialScale: 1,
-  viewportFit: "cover",
+	width: "device-width",
+	initialScale: 1,
+	viewportFit: "cover",
 }
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body className="bg-gray-900 antialiased">
-        {children}
-      </body>
-    </html>
-  )
+	return (
+		<html lang="en">
+			<body className="bg-gray-900 antialiased">
+				{children}
+			</body>
+		</html>
+	)
 }
