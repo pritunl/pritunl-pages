a36adda58bd3faa598c7ee03b6b5a67fad12c0d4
Zachary Huff <zach.huff.386@gmail.com>
2026-03-11 11:38:46 -0400

    Use config in app page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,4 @@
-import type { ProductDefinition } from "./types"
-
-import cloud from "./cloud/config"
-
-const products: Record<string, ProductDefinition> = {
-	cloud,
-}
-
-const product = products[process.env.PRODUCT as string]
+import product from "./config"
 
 export default function RootPage() {
 	const HomePage = product.pages.home
