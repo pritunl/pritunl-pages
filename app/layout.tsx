d7667f755ca3ca2da2925615727c33740eef0920
Zachary Huff <zach.huff.386@gmail.com>
2026-03-28 18:23:06 -0400

    Add scripts to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata, Viewport } from "next"
+import Script from "next/script"
 
 import product from "./config"
 
@@ -42,6 +43,22 @@ export default function RootLayout({
 		<html lang="en">
 			<body className="bg-gray-950 antialiased">
 				{children}
+				{config.scripts?.map((script, i) => {
+					const dataProps: Record<string, string> = {}
+					if (script.dataAttributes) {
+						for (const [key, value] of Object.entries(script.dataAttributes)) {
+							dataProps[`data-${key}`] = value
+						}
+					}
+					return (
+						<Script
+							key={i}
+							src={script.src}
+							strategy={script.defer ? "afterInteractive" : "beforeInteractive"}
+							{...dataProps}
+						/>
+					)
+				})}
 			</body>
 		</html>
 	)
