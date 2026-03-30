777676609088ccfed2b7b89c80bc0a1e5eef08a0
Zachary Huff <zach.huff.386@gmail.com>
2026-03-30 18:22:27 -0400

    Add client to app config

--- app/config.ts
+++ app/config.ts
@@ -2,10 +2,12 @@ import type { ProductDefinition } from "./types"
 
 import cloud from "./cloud/config"
 import zero from "./zero/config"
+import client from "./client/config"
 
 const products: Record<string, ProductDefinition> = {
 	cloud,
 	zero,
+	client,
 }
 
 const product = products[process.env.NEXT_PUBLIC_PRODUCT as string]
