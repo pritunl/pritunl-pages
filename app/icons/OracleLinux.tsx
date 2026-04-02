bf0e28d246b5f5423c72b26b3bd9319418350513
Zachary Huff <zach.huff.386@gmail.com>
2026-04-02 19:24:21 -0400

    Add oracle linux icon

---
+++ app/icons/OracleLinux.tsx
@@ -0,0 +1,9 @@
+import type { SVGProps } from "react"
+
+export default function OracleLinux(props: SVGProps<SVGSVGElement>) {
+	return (
+		<svg role="img" viewBox="0 0 32 20" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" strokeWidth="4" {...props}>
+			<path d="M10 2a8 8 0 1 0 0 16h12a8 8 0 1 0 0-16z"/>
+		</svg>
+	)
+}
