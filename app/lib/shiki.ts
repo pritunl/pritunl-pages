7af6aae13946b8176dade9d75f4d0940db8e6cec
Zachary Huff <zach.huff.386@gmail.com>
2026-04-05 18:51:05 -0400

    Update highlighter in shiki

--- app/lib/shiki.ts
+++ app/lib/shiki.ts
@@ -1,13 +1,17 @@
-import { createHighlighter } from "shiki"
+import { createHighlighterCore } from "shiki/core"
+import { createJavaScriptRegexEngine } from "shiki/engine/javascript"
+import githubDark from "shiki/themes/github-dark.mjs"
+import langBash from "shiki/langs/bash.mjs"
 
-let highlighter: Awaited<ReturnType<typeof createHighlighter>> | null = null
+let highlighter: Awaited<ReturnType<typeof createHighlighterCore>> | null = null
 
 export async function highlight(code: string, lang: string) {
-	if (!highlighter) {
-		highlighter = await createHighlighter({
-			themes: ["github-dark"],
-			langs: ["bash"],
-		})
-	}
-	return highlighter.codeToHtml(code, { lang, theme: "github-dark" })
+    if (!highlighter) {
+        highlighter = await createHighlighterCore({
+            themes: [githubDark],
+            langs: [langBash],
+            engine: createJavaScriptRegexEngine(),
+        })
+    }
+    return highlighter.codeToHtml(code, { lang, theme: "github-dark" })
 }
