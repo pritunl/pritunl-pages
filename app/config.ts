f4029c7db4387a0253a98abe55ab468e01bc2ba6
Zachary Huff <zach.huff.386@gmail.com>
2026-04-07 13:56:33 -0400

    Add vpn to app config

--- app/config.ts
+++ app/config.ts
@@ -3,11 +3,13 @@ import type { ProductDefinition } from "./types"
 import cloud from "./cloud/config"
 import zero from "./zero/config"
 import client from "./client/config"
+import vpn from "./vpn/config"
 
 const products: Record<string, ProductDefinition> = {
 	cloud,
 	zero,
 	client,
+	vpn,
 }
 
 const product = products[process.env.NEXT_PUBLIC_PRODUCT as string]
