2742c443bce00965d7cef16ce4a42c2380edda5d
Zachary Huff <zach.huff.386@gmail.com>
2026-04-11 13:01:10 -0400

    Add footer to vpn config

--- app/vpn/config.ts
+++ app/vpn/config.ts
@@ -1,6 +1,7 @@
 import type { ProductDefinition } from "../types"
 
 import Home from "../components/Home"
+import Pritunl from "../logos/Pritunl"
 
 const definition: ProductDefinition = {
 	config: {
@@ -39,6 +40,21 @@ const definition: ProductDefinition = {
 				external: true,
 			},
 		],
+
+		footer: {
+			icon: Pritunl,
+			address1: "1700 7th Ave Ste 116-211",
+			address2: "Seattle, WA 98101",
+			phone: "(206) 202-2276",
+			copyright: `Pritunl, Inc © ${new Date().getFullYear()}`,
+			privacy: "https://pritunl.com/privacy",
+			terms: "https://pritunl.com/terms",
+			twitter: "https://x.com/pritunl",
+			bluesky: "https://bsky.app/profile/pritunl.bsky.social",
+			github: "https://github.com/pritunl/pritunl-client",
+			linkedin: "https://linkedin.com/company/pritunl",
+			substack: "https://pritunl.substack.com",
+		},
 	},
 
 	pages: {
