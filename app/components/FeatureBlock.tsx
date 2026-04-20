0bac89a5628738583fc34621bd127c81cf525f5a
Zachary Huff <zach.huff.386@gmail.com>
2026-04-20 17:18:58 -0400

    Update link labels in feature block component

--- app/components/FeatureBlock.tsx
+++ app/components/FeatureBlock.tsx
@@ -81,11 +81,11 @@ export default function FeatureBlock({ block, first, last }: Props) {
 													<p className="mt-6">
 														{item.external ? (
 															<a href={item.link} target="_blank" rel="noopener noreferrer" className="text-base font-semibold text-indigo-400 hover:text-indigo-300">
-																{item.linkLabel || "Learn more"} <span aria-hidden="true">→</span>
+																{item.linkLabel} <span aria-hidden="true">→</span>
 															</a>
 														) : (
 															<Link href={item.link} className="text-base font-semibold text-indigo-400 hover:text-indigo-300">
-																{item.linkLabel || "Learn more"} <span aria-hidden="true">→</span>
+																{item.linkLabel} <span aria-hidden="true">→</span>
 															</Link>
 														)}
 													</p>
